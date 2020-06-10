8d1ce5f01c8dd6b5026761345a153d72d825d7ab
Nathan Cowley <nathancowley12@gmail.com>
2020-06-10 08:11:00 -0600

    MVP

--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import AdditionalFeature from './AdditionalFeature';
+import { connect } from 'react-redux'
+import {addFeature} from '../actions/featureAction'
+
+
 
 const AdditionalFeatures = props => {
+
+  const addItem = item => {
+    props.addFeature(item)
+  };
+
   return (
     <div className="content">
       <h4>Additional Features</h4>
       {props.additionalFeatures.length ? (
         <ol type="1">
           {props.additionalFeatures.map(item => (
-            <AdditionalFeature key={item.id} feature={item} />
+            <AdditionalFeature addFeature={addItem} key={item.id} feature={item} />
           ))}
         </ol>
       ) : (
@@ -18,4 +27,11 @@ const AdditionalFeatures = props => {
   );
 };
 
-export default AdditionalFeatures;
+
+const mapStateToProps = state => {
+  return {
+    additionalFeatures: state.additionalFeatures
+  }
+}
+
+export default connect(mapStateToProps, {addFeature})(AdditionalFeatures);
